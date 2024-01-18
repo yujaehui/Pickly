6de4f20920b7e98f520cc877648be843a5a6a0d7
Jaehui Yu <pkj0522@naver.com>
2024-01-19 01:35:10 +0900

    Initial Commit

---
+++ Pickly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pickly
+//
+//  Created by Jaehui Yu on 2/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
