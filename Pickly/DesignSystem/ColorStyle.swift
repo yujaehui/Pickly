6cb66103d5ba819498ffbcab620fee0676d934b6
Jaehui Yu <pkj0522@naver.com>
2024-01-19 15:17:00 +0900

    feat: Implement Design System - Added ColorStyle for consistent color scheme - Added FontStyle for typography standardization - Added ImageStyle with predefined assets - Ensured UI consistency across the project

---
+++ Pickly/DesignSystem/ColorStyle.swift
@@ -0,0 +1,14 @@
+//
+//  ColorStyle.swift
+//  Pickly
+//
+//  Created by Jaehui Yu on 1/19/24.
+//
+
+import UIKit
+
+enum ColorStyle {
+    static let point = UIColor(hexCode: "#49DC92")
+    static let text = UIColor(hexCode: "#FFFFFF")
+    static let background = UIColor(hexCode: "#000092")
+}
---
+++ Pickly/DesignSystem/ImageStyle.swift
@@ -0,0 +1,28 @@
+//
+//  ImageStyle.swift
+//  Pickly
+//
+//  Created by Jaehui Yu on 1/19/24.
+//
+
+import UIKit
+
+enum ImageStyle {
+    static let profile1: UIImage = .profile1
+    static let profile2: UIImage = .profile2
+    static let profile3: UIImage = .profile3
+    static let profile4: UIImage = .profile4
+    static let profile5: UIImage = .profile5
+    static let profile6: UIImage = .profile6
+    static let profile7: UIImage = .profile7
+    static let profile8: UIImage = .profile8
+    static let profile9: UIImage = .profile9
+    static let profile10: UIImage = .profile10
+    static let profile11: UIImage = .profile11
+    static let profile12: UIImage = .profile12
+    static let profile13: UIImage = .profile13
+    static let profile14: UIImage = .profile14
+    static let camera: UIImage = .camera
+    static let onboarding: UIImage = .onboarding
+    static let empty: UIImage = .empty
+}
