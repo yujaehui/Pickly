9d64fefdd740f71b661a897c7944462a48977ae6
Jaehui Yu <pkj0522@naver.com>
2024-01-20 09:34:11 +0900

    chore: Update background color to black

--- Pickly/DesignSystem/ColorStyle.swift
+++ Pickly/DesignSystem/ColorStyle.swift
@@ -10,5 +10,5 @@ import UIKit
 enum ColorStyle {
     static let point = UIColor(hexCode: "#49DC92")
     static let text = UIColor(hexCode: "#FFFFFF")
-    static let background = UIColor(hexCode: "#000092")
+    static let background = UIColor(hexCode: "#000000")
 }
