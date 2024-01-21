96d4d643f407297657aea355d71adfc215c5f104
Jaehui Yu <pkj0522@naver.com>
2024-01-21 13:59:24 +0900

    fix: Call setNavigation in BaseViewController's viewDidLoad

--- Pickly/Base/BaseViewController.swift
+++ Pickly/Base/BaseViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNavigation()
         configureView()
     }
     
